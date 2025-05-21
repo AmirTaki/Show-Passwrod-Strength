49b1310bf4afdc4569d61b13cb785501dae08c8f
amir <amirtaki623@gmail.com>
2025-05-21 20:07:44 +0330

    js : full

--- src/main.js
+++ src/main.js
@@ -9,16 +9,25 @@ const change = (value) => {
     }
     else {
         msg.style.display  = 'none'
+       
     }
-    if (value < 4){
+    if (value == 0){
+         pass.style.borderColor = "#ccc"
+    }
+    else if (value < 4){
         str.innerHTML = "weak"
-        pass.style.borderColor = ''
+        pass.style.borderColor = '#ff5925'
+        msg.style.color = '#ff5925'
     }
     else if  (value >= 4 && value< 8){
         str.innerHTML = 'medum'
+        pass.style.borderColor = 'yellow'
+        msg.style.color = 'yellow'
     }
     else if (value >= 8) {
         str.innerHTML = "strong"
+        pass.style.borderColor = '#26d730'
+        msg.style.color = '#26d730'
     }
 }
 
